4208c226076538e9cc8f3036116f86938fc395e7
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-07-27 22:05:15 +0300

    Create form

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Header from "./components/header";
 import Main from "./components/main";
 import Footer from "./components/footer";
 
-// Create a SelectedBeast component and include it in your App
-// Use the state in the App to render an individual beast in a Modal in the SelectedBeast component using React Bootstrap
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
